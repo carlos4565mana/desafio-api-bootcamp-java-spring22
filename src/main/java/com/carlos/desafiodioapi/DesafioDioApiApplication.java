7acfc3d58858ba90fc81eb6c473c42a8b9f8aae9
carlos4565mana <carloscal61@gmail.com>
2023-10-21 15:59:23 -0200

    first commit

---
+++ src/main/java/com/carlos/desafiodioapi/DesafioDioApiApplication.java
@@ -0,0 +1,13 @@
+package com.carlos.desafiodioapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioDioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioDioApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carlos/desafiodioapi/DesafioDioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlos.desafiodioapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioDioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
