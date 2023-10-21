25c4b9c71d5ae60e18489caabecff8ee30d390cd
carlos4565mana <carloscal61@gmail.com>
2023-10-21 17:20:14 -0200

    database seed

--- src/main/java/com/carlos/desafiodioapi/entities/Score.java
+++ src/main/java/com/carlos/desafiodioapi/entities/Score.java
@@ -1,5 +1,44 @@
 package com.carlos.desafiodioapi.entities;
 
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="tb_score")
 public class Score {
 
+	@EmbeddedId
+	private ScorePk id = new ScorePk();
+	private Double value;
+	
+	public Score() {
+
+	}
+	
+	public void setMovie(Movie movie) {
+		id.setMovie(movie);
+	}
+
+	public void setUser(User user) {
+		id.setUser(user);
+	}
+	
+	public ScorePk getId() {
+		return id;
+	}
+
+	public void setId(ScorePk id) {
+		this.id = id;
+	}
+
+	public Double getValue() {
+		return value;
+	}
+
+	public void setValue(Double value) {
+		this.value = value;
+	}
+	
+	
 }
--- src/main/java/com/carlos/desafiodioapi/entities/ScorePk.java
+++ src/main/java/com/carlos/desafiodioapi/entities/ScorePk.java
@@ -1,5 +1,47 @@
 package com.carlos.desafiodioapi.entities;
 
-public class ScorePk {
+import java.io.Serializable;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Embeddable
+public class ScorePk implements Serializable{
+
+	
+	private static final long serialVersionUID = 1L;
+	
+	@ManyToOne
+	@JoinColumn(name = "movie_id")
+	private Movie movie;
+	
+	
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+	
+	public ScorePk() {
+
+	}
+	
+	public Movie getMovie() {
+		return movie;
+	}
+
+	public void setMovie(Movie movie) {
+		this.movie = movie;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	
+	
 
 }
